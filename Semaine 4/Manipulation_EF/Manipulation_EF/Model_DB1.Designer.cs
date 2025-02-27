bf91bf43b18c7310146d5529aed8c5d7d97f9dba
Yanenchh25 <johanednoham@gmail.com>
2025-02-27 16:02:35 -0500

    fusion

--- Semaine 4/Manipulation_EF/Manipulation_EF/Model_DB1.Designer.cs
+++ Semaine 4/Manipulation_EF/Manipulation_EF/Model_DB1.Designer.cs
@@ -1,4 +1,5 @@
 ﻿// La génération de code T4 est activée pour le modèle 'D:\Cours H2022\4DB\Lectures\LE03 - Manipuler l'EDM\Manipulation_EF\Manipulation_EF\Model_DB1.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\SESSION 4\Developpement BD\consoles\Exercices\420-4DB-HY-H25\Semaine 4\Manipulation_EF\Manipulation_EF\Model_DB1.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
